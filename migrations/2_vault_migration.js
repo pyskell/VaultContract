027450489eb79c93d0533f01144a31f6f5b7ea6a
pyskell <pyskell@pyskell>
2017-06-13 16:20:31 -0400

    migrations appear to be working

--- migrations/2_vault_migration.js
+++ migrations/2_vault_migration.js
@@ -1,5 +1,11 @@
 var VaultContract = artifacts.require("Vault");
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 
 module.exports = function(deployer){
-    deployer.deploy(VaultContract);
+    var mnemonic = "avoid aunt cool tent speed skull pony radio deposit tornado country wrestle";
+    // var managerAddress = HDWalletProvider(mnemonic, "http://localhost:8545", 0);
+    var panicAddress = HDWalletProvider(mnemonic, "http://localhost:8545", 1);
+
+    deployer.deploy(VaultContract, panicAddress, 900, 1);
 }
--- truffle.js
+++ truffle.js
@@ -1,17 +1,9 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-
-var mnemonic = "avoid aunt cool tent speed skull pony radio deposit tornado country wrestle";
-
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    ropsten: {
-      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
-      network_id: 3
     }
   }
 };
